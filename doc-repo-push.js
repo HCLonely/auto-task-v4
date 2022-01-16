161dfd8d2f27e93fb832b5a700abe8079211c7fe
HCLonely <h1606051253@gmail.com>
2022-01-16 18:24:24 +0800

    文档同步测试

--- doc-repo-push.js
+++ doc-repo-push.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : HCLonely
  * @Date         : 2022-01-16 17:31:53
- * @LastEditTime : 2022-01-16 18:19:37
+ * @LastEditTime : 2022-01-16 18:24:00
  * @LastEditors  : HCLonely
  * @FilePath     : /auto-task-new/doc-repo-push.js
  * @Description  : 同步auto-task-doc仓库
@@ -18,9 +18,10 @@ const git = (...args) => {
 stderr:
   ${stderr}`);
 };
+git('config', '--global', 'init.defaultBranch', 'gh-pages');
+git('config', '--global', 'user.name', 'HCLonely');
+git('config', '--global', 'user.email', 'h1606051253@gmail.com');
 git('init');
-git('config', 'user.name', 'HCLonely');
-git('config', 'user.email', 'h1606051253@gmail.com');
 git('add', '-A');
 git('commit', '-m', `Update at ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`);
 git('push', '-u', 'https://github.com/HCLonely/auto-task-doc', 'HEAD:gh-pages', '--force');
