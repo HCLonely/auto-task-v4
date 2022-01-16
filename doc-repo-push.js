e68728e63ec88942fdfa7abe79ad10697eef2811
HCLonely <h1606051253@gmail.com>
2022-01-16 18:18:12 +0800

    文档同步测试

--- doc-repo-push.js
+++ doc-repo-push.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : HCLonely
  * @Date         : 2022-01-16 17:31:53
- * @LastEditTime : 2022-01-16 18:06:52
+ * @LastEditTime : 2022-01-16 18:17:07
  * @LastEditors  : HCLonely
  * @FilePath     : /auto-task-new/doc-repo-push.js
  * @Description  : 同步auto-task-doc仓库
@@ -11,9 +11,16 @@
 const { spawnSync } = require('child_process');
 const dayjs = require('dayjs');
 
-console.log(spawnSync('git', ['init'], { cwd: 'doc' }));
-console.log(spawnSync('git', ['config', 'user.name', 'Doc Sync'], { cwd: 'doc' }));
-console.log(spawnSync('git', ['config', 'user.email', 'h1606051253@gmail.com'], { cwd: 'doc' }));
-console.log(spawnSync('git', ['add', '-A'], { cwd: 'doc' }));
-console.log(spawnSync('git', ['commit', '-m', `Update at ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`], { cwd: 'doc' }));
-console.log(spawnSync('git', ['push', '-u', 'https://github.com/HCLonely/auto-task-doc', 'HEAD:gh-pages', '--force'], { cwd: 'doc' }));
+const git = (...args) => {
+  const { stdout, stderr } = spawnSync('git', args, { cwd: 'doc' });
+  console.log(`stdout:
+  ${stdout}
+stderr:
+  ${stderr}`);
+};
+git('init');
+git('config', 'user.name', 'Doc Sync');
+git('config', 'user.email', 'h1606051253@gmail.com');
+git('add', '-A');
+git('commit', '-m', `Update at ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`);
+git('push', '-u', 'https://github.com/HCLonely/auto-task-doc', 'HEAD:gh-pages', '--force');
