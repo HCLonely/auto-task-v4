7ddb1d956558744c5396684cfb28d96ba1117ee5
HCLonely <h1606051253@gmail.com>
2022-01-16 17:50:09 +0800

    文档同步测试

--- doc-repo-push.js
+++ doc-repo-push.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : HCLonely
  * @Date         : 2022-01-16 17:31:53
- * @LastEditTime : 2022-01-16 17:40:01
+ * @LastEditTime : 2022-01-16 17:50:06
  * @LastEditors  : HCLonely
  * @FilePath     : /auto-task-new/doc-repo-push.js
  * @Description  : 同步auto-task-doc仓库
@@ -16,5 +16,5 @@ spawnSync('git', ['init']);
 spawnSync('git', ['config', 'user.name', 'Doc Sync']);
 spawnSync('git', ['config', 'user.email', 'h1606051253@gmail.com']);
 spawnSync('git', ['add', '-A']);
-spawnSync('git', ['commit', '-m', `Update at ${dayjs.format('YYYY-MM-DD HH:mm:ss')}`]);
+spawnSync('git', ['commit', '-m', `Update at ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`]);
 spawnSync('git', ['push', '-u', 'https://github.com/HCLonely/auto-task-doc', 'HEAD:gh-pages', '--force']);
